88918b414638095270468bcfb94ed3443b72b86c
Prince-Japheth <japhetjerryjj@gmail.com>
2025-02-20 00:48:45 +0100

    Changed title & Meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Japheth - Software Engineer</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
