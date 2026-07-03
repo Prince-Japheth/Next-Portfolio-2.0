e4d1353d7558bb06e947263d25f2906dd377383f
Prince-Japheth <japhethjerryjj@gmail.com>
2026-07-03 02:58:21 +0100

    style(projects page): add missing trailing newline

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -21,4 +21,4 @@ export default async function Projects(props: PageProps) {
       />
     </Suspense>
   );
-}
+}                                                                                                                                                                   
