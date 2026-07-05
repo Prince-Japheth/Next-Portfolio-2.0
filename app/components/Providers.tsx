f67a775fbebe58edcdd9b420b91524014dfef50f
Prince-Japheth <japhethjerryjj@gmail.com>
2026-07-05 12:27:42 +0100

    feat: integrate Lenis smooth scrolling and remove redundant CSS styles

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -4,13 +4,16 @@ import React from "react"
 import { BrowserProvider } from "../context/BrowserContext"
 import { LoadingProvider } from "../context/LoadingContext"
 import { WordpressProvider } from "../providers/WordpressProvider"
+import { LenisProvider } from "../providers/LenisProvider"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <LoadingProvider>
       <BrowserProvider>
         <WordpressProvider>
-          {children}
+          <LenisProvider>
+            {children}
+          </LenisProvider>
         </WordpressProvider>
       </BrowserProvider>
     </LoadingProvider>
